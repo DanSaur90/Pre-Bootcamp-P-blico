5b704c2792e16e7bda3a4bd2fe0a7f6905d09341
DanielJaramillo <daniel809462@gmail.com>
2022-04-11 22:24:44 -0500

    Enter

---
+++ ninja.js
@@ -0,0 +1,21 @@
+class Ninja {
+    constructor(nombre, salud, velocidad, fuerza){
+        this.nombre=nombre;
+        this.salud=salud;
+        this.velocidad=3;
+        this.fuerza=3;
+    }
+    sayName(){
+        console.log(this.nombre);
+    }
+    showStats(){
+        console.log(`El nombre del ninja es  ${this.name} su fuerza es ${this.fuerza} su velocidad es ${this.velocidad} y su salud es ${this.salud}`);
+    }
+    drinkSake(){
+        this.salud+=10
+    }
+}
+const ninja1 = new Ninja("Hyabusa",100);
+ninja1.sayName();
+ninja1.showStats();
+ninja1.drinkSake();
